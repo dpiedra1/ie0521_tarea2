8c67add10cc94b42a2a7a22c35fd6bea99148fdd
Daniel Piedra <danielpiedra25@hotmail.com>
2018-06-27 22:55:04 -0600

    Change how work is distributed in exercise 1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,149 +1,234 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_ROWS 6
-#define NUM_COLS 5
-
 
 int main(int argc, char *argv[]){
 
-  int rank, total_processes;
-  MPI_Init(&argc, &argv);
-
+  // In this test root process is one that has rank 0
   int root = 0;
 
   // World communication
   MPI_Comm comm = MPI_COMM_WORLD;
 
+  int rank, total_processes;
+  MPI_Init(&argc, &argv);
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
 
-  if(rank == 0){
-    // Define matrix and 1D vector only in root process
+  // Create array only if root process
+  // In this case array size is the same as number of columns
+  int array_size;
+  int num_row;
+
+  // Array for slave process
+  float *recv_array;
+  int *indexes;
+
+  // 1D Matrix
+  float *matrix_1D;
+
+  // Define a buffer to receive data
+  int *recvbuf;
+  int recvcount;
+
+  // Result
+  float *result;
+
+  // We need two int arrays to specify number of elements to each process and
+  // the offset
+  int *sendcounts = (int *)malloc(total_processes*sizeof(int));
+  int *displs = (int *)malloc(total_processes*sizeof(int));
+
+  if(root == rank){
+
+    // Define matrix and 1D array only in root process
     // ********************************************************************
-    float result[NUM_ROWS];
+    float array[5] = {1.0,1.0,1.0,1.0,1.0};
+    float matrix[6][5] = {{1, 1, 1, 1, 1},
+                          {2, 2, 2, 2, 2},
+                          {3, 3, 3, 3, 3},
+                          {4, 4, 4, 4, 4},
+                          {5, 5, 5, 5, 5},
+                          {6, 6, 6, 6, 6}};
 
-    float vector[NUM_COLS] = {1.0,1.0,1.0,1.0,1.0};
-    float matrix[NUM_ROWS][NUM_COLS] = {{1, 1, 1, 1, 1},
-                                        {2, 2, 2, 2, 2},
-                                        {3, 3, 3, 3, 3},
-                                        {4, 4, 4, 4, 4},
-                                        {5, 5, 5, 5, 5},
-                                        {6, 6, 6, 6, 6}};
+    // Get data from both arrays
+    array_size = sizeof(array)/sizeof(float);
+    num_row = sizeof(matrix) / sizeof(matrix[0]);
 
-    // Get matrix and vector dimensions
-    // ********************************************************************
-    // Vector and matrix could be of different dimensions. We do not want
-    // to hardcode this values (in fact they are, but suppose they are not)
-    // so if we use NUM_COLS and NUM_ROWS then we are cheating. That is why we
-    // get this values again.
-    // In this case vector size is the same as column size, since we suppose
-    // that this is a multiplication of matrix by column vector
-    int num_cols = sizeof(vector)/sizeof(float);
-    int num_rows = sizeof(matrix) / sizeof(matrix[0]);
+    // Broadcast array size to other process
+    MPI_Bcast(&array_size, 1, MPI_INT, root, comm);
 
-    // Broadcast vector and matrix size
-    MPI_Bcast(&num_cols, 1, MPI_INT, root, comm);
-    MPI_Bcast(&num_rows, 1, MPI_INT, root, comm);
+    // Broadcast number of rows to other process
+    MPI_Bcast(&num_row, 1, MPI_INT, root, comm);
 
-    // Broadcast vector
-    MPI_Bcast(vector, num_cols, MPI_FLOAT, root, comm);
+    // Broadcast array to other processes
+    MPI_Bcast(array, array_size, MPI_FLOAT, root, comm);
 
-    int answer;
-    float result_value;
-    int message = 1;
-    MPI_Status status;
-
-    // Distribute work among slave processes
-    // ----------------------------------------------------------------------
-    // Since root process is sending several messages to same slave process,
-    // this can cause that messages are not sended or received in expected
-    // order (this is a huge problem since we expect them in order).
-    // That is why we use tags. Sometimes is sayed that tags are optional, but
-    // in this case they are not. If we use the same tag (for example 0) then
-    // most of the times nothing works.
-    for(int row_number=0; row_number<num_rows;row_number++){
-      MPI_Recv(&answer,1, MPI_INT, MPI_ANY_SOURCE, 0, comm, &status);
-
-      // Send a message with row number of the matrix (value different from -1)
-      message = row_number;
-      MPI_Send(&message, 1, MPI_INT, status.MPI_SOURCE, 1, comm);
-
-      // Send row
-      MPI_Send(matrix[row_number], num_cols, MPI_FLOAT, status.MPI_SOURCE, 2, comm);
-
-      // Receive result
-      MPI_Recv(&result_value,1, MPI_FLOAT, MPI_ANY_SOURCE, 3, comm, &status);
-      result[row_number] = result_value;
+    // Convert matrix to 1D array
+    matrix_1D = (float*)malloc(array_size*num_row*sizeof(float));
+    for(int i=0;i<num_row;i++){
+      for(int j=0;j<array_size;j++){
+        matrix_1D[i*array_size + j] = matrix[i][j];
+      }
     }
 
-    message = -1;
-    for(int process=1; process<total_processes; process++){
-      MPI_Send(&message, 1, MPI_INT, process, 1, comm);
-    }
+    // Broadcast matrix to all processes
+    MPI_Bcast(matrix_1D, array_size*num_row, MPI_FLOAT, root, comm);
 
-    // Print result
-    printf("Printing result...\n");
-    for(int i =0; i<num_rows; i++){
-      if(i != num_rows-1){
-        printf("%f, ", result[i]);
+    int remainder = num_row % total_processes;
+
+    int whole_part = (num_row - remainder) / total_processes;
+
+    // Distribute whole part
+    for(int i=0; i < total_processes; i++){
+      sendcounts[i] = whole_part;
+    }
+    // Distribute remainder
+    for(int i=1; i < total_processes; i++){
+      if(remainder == 0){
+        break;
       }
       else{
-        printf("%f", result[i]);
+        sendcounts[i]++;
+        remainder--;
       }
     }
-    printf("\n");
 
-  }else{
-    int num_cols, num_rows;
-    // Receive vector and matrix size
-    MPI_Bcast(&num_cols, 1, MPI_INT, root, comm);
-    MPI_Bcast(&num_rows, 1, MPI_INT, root, comm);
+    // Root process has zero offset
+    displs[0] = 0;
 
-    // Receive vector
-    float *vector;
-    vector = (float *)malloc(num_cols*sizeof(float));
-    MPI_Bcast(vector, num_cols, MPI_FLOAT, root, comm);
+    // Assign offset to other processes
+    for(int i=1; i < total_processes; i++){
+      displs[i] = displs[i-1] + sendcounts[i-1];
+    }
 
-    // Allocate memory for specific row
-    float *row;
-    row = (float *)malloc(num_cols*sizeof(float));
+    recvbuf = (int *)malloc(sendcounts[rank]*sizeof(int));
+    recvcount = sendcounts[rank];
 
-    int answer;
-    int message = 1;
+    // Indexes of the matrix
+    // **************************************************************************
+    indexes = (int*)malloc(num_row*sizeof(int));
+    for(int i=0;i<num_row;i++){
+      indexes[i] =i;
+    }
+    MPI_Scatterv(indexes, sendcounts, displs, MPI_INT, recvbuf, recvcount,
+                 MPI_INT, root, comm);
 
-    // Receive work from master process
-    // ----------------------------------------------------------------------
-    while(1){
-      MPI_Send(&message, 1, MPI_INT, 0, 0, comm);
-      MPI_Recv(&answer,1, MPI_INT, 0, 1, comm, MPI_STATUS_IGNORE);
-      if(answer == -1){
-        break;
-      }else{
-        // Receive row
-        MPI_Recv(row, num_cols, MPI_FLOAT, 0, 2, comm, MPI_STATUS_IGNORE);
-
-        // Calculate result
-        float result_entry = 0;
-        for(int i=0;i<num_cols; i++){
-          result_entry = result_entry + row[i]*vector[i];
-        }
-        // Send result
-        MPI_Send(&result_entry, 1, MPI_FLOAT, 0, 3, comm);
-
-        // Print the work that each slave process has done
-        printf("I am %d and my result is: %f\n", rank, result_entry);
-
+    // Make calcs
+    // **************************************************************************
+    result = (float*)malloc(recvcount*sizeof(float));
+    for(int i = 0; i<recvcount; i++){
+      float sum = 0.0;
+      int index = recvbuf[i];
+      for(int j=0; j<array_size; j++){
+        sum = sum + matrix_1D[index*array_size + j]*array[j];
       }
+      result[i] = sum;
+    }
+
+    // **************************************************************************
+
+    // Return data to root process
+    // **************************************************************************
+    float *final_result;
+    final_result = (float *)malloc(num_row*sizeof(float));
+
+    MPI_Gatherv(result, recvcount, MPI_FLOAT, final_result, sendcounts, displs,
+               MPI_FLOAT, root, comm);
+
+    printf("Printing final result..\n" );
+    for(int i=0; i<num_row; i++){
+      printf("%f\n", final_result[i]);
     }
 
     // Free memory
-    free(row);
-    free(vector);
+    free(final_result);
+    free(indexes);
+
 
   }
+  // When non root process
+  else{
+    // Get array size from root process
+    MPI_Bcast(&array_size, 1, MPI_FLOAT, root, comm);
+
+    // Get number of rows of matrix from root process
+    MPI_Bcast(&num_row, 1, MPI_INT, root, comm);
+
+    // Receive array
+    recv_array = (float*)malloc(array_size*sizeof(float));
+    MPI_Bcast(recv_array, array_size, MPI_FLOAT, root, comm);
+
+    // Broadcast matrix to all processes
+    matrix_1D = (float*)malloc(array_size*num_row*sizeof(float));
+    MPI_Bcast(matrix_1D, array_size*num_row, MPI_FLOAT, root, comm);
+
+    int remainder = num_row % total_processes;
+
+    int whole_part = (num_row - remainder) / total_processes;
+
+    // Distribute whole part
+    for(int i=0; i < total_processes; i++){
+      sendcounts[i] = whole_part;
+    }
+    // Distribute remainder
+    for(int i=1; i < total_processes; i++){
+      if(remainder == 0){
+        break;
+      }
+      else{
+        sendcounts[i]++;
+        remainder--;
+      }
+    }
+
+    // Root process has zero offset
+    displs[0] = 0;
+
+    // Assing offset to other processes
+    for(int i=1; i < total_processes; i++){
+      displs[i] = displs[i-1] + sendcounts[i-1];
+    }
+
+    recvbuf = (int *)malloc(sendcounts[rank]*sizeof(int));
+    recvcount = sendcounts[rank];
+
+    // Distribute data
+    // **************************************************************************
+    MPI_Scatterv(NULL, sendcounts, displs, MPI_INT, recvbuf, recvcount,
+                 MPI_INT, root, comm);
+
+    // Make calcs
+    // **************************************************************************
+    result = (float*)malloc(recvcount*sizeof(float));
+    for(int i = 0; i<recvcount; i++){
+     float sum = 0.0;
+     int index = recvbuf[i];
+     for(int j=0; j<array_size; j++){
+       sum = sum + matrix_1D[index*array_size + j]*recv_array[j];
+     }
+      result[i] = sum;
+    }
+
+    // **************************************************************************
+
+    // Return data to root process
+    // **************************************************************************
+
+    MPI_Gatherv(result, recvcount, MPI_FLOAT, NULL, sendcounts, displs,
+               MPI_FLOAT, root, comm);
+  }
+
+  // Free memory
+  // **************************************************************************
+  free(sendcounts);
+  free(displs);
+  free(matrix_1D);
+  free(recvbuf);
+  free(result);
+  free(recv_array);
 
   MPI_Finalize();
   return 0;
-
 }
